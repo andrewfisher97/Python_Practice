7e602ecb24fc4b15d0ca9cafb62a99a05737eeb7
Andrew Fisher <andrew.c.fisher97@gmail.com>
2022-02-24 15:52:37 -0500

    simple fix

--- character_input.py
+++ character_input.py
@@ -2,16 +2,15 @@
 # it then prints out a message that tells them the year that they will turn 100 years old
 # it also asks them for their favourite number and repeats the message that many times
 
+
 def year_when_one_hundred(user_birth_year):
     return user_birth_year + 100
 
 
 if __name__ == '__main__':
     name = input("Enter your name: ")
-    birth_year = input("Enter your birth year: ")
-    birth_year = int(birth_year)
-    number = input("Enter your favourite number: ")
-    number = int(number)
+    birth_year = int(input("Enter your birth year: "))
+    number = int(input("Enter your favourite number: "))
 
     while number > 0:
         print(f"The year is {year_when_one_hundred(birth_year)} when you are 100 years old!")
